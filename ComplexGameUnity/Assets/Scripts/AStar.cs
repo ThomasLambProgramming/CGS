b7e9a0574957bac37ba8c8e321d3121120d9918a
Thomas Lamb <s200498@students.aie.edu.au>
2021-04-29 15:43:06 +1000

    Working aStar pathfinding

--- ComplexGameUnity/Assets/Scripts/AStar.cs
+++ ComplexGameUnity/Assets/Scripts/AStar.cs
@@ -8,19 +8,20 @@ public class AStar : MonoBehaviour
 {
     class PathNode
     {
-        float m_startDistance = 0;
-        float m_endDistance = 0;
-        float m_fCost {get { return m_startDistance + m_endDistance; } }
-        Node m_node = null;
-        PathNode m_parent = null;
+        public float m_totalCost = 0;
+        public float m_gCost = 0;
+        public float m_hCost = 0;
+        public float m_fCost {get { return m_gCost + m_hCost; } }
+        public Node m_node = null;
+        public PathNode m_parent = null;
 
-        PathNode(Node a_node, PathNode a_parent)
+        public PathNode(Node a_node, PathNode a_parent)
         {
             m_node = a_node;
             m_parent = a_parent;
         }
     }
-    static Node FindClosestNode(Vector3 a_position)
+    public static Node FindClosestNode(Vector3 a_position)
     {
         if (NodeManager.m_nodeGraph == null)
             return null;
@@ -38,35 +39,96 @@ public class AStar : MonoBehaviour
         }
         return closestNode;
     }
-    public static void Pathfind(Vector3 a_startPos, Vector3 a_endPos)
+    public static List<Vector3> Pathfind(Vector3 a_startPos, Vector3 a_endPos)
     {
-        List<Node> openNodes = new List<Node>();
-        List<Node> closedNodes = new List<Node>();
+        List<PathNode> openNodes = new List<PathNode>();
+        List<PathNode> closedNodes = new List<PathNode>();
         
+        //find the closest node from the start and finish, after the path is found there will be an additional
+        //check that can be performed to see if the point can be reached after the path (eg the closest may be at the
+        //start of a mountain but there are no nodes and the end point is the peak)
         Node startNode = FindClosestNode(a_startPos);
         Node endNode = FindClosestNode(a_endPos);
 
-        openNodes.Add(startNode);
+        //giving it null as it is the starting node
+        PathNode start = new PathNode(startNode, null);
+        start.m_gCost = 0;
+        start.m_hCost = Vector3.Distance(start.m_node.m_position, endNode.m_position);
+        openNodes.Add(start);
 
-        bool isSearching = true;
-        while(isSearching)
+       
+        while(openNodes.Count > 0)
         {
-            foreach(var node in openNodes)
-            {
-                //find the one with the lowest f cost
-            }
-            //current node = lowest one found
-            Node currentNode = null;
-            //openNodes.Remove(currentNode);
-            //closedNodes.Add(currentNode);
-            if (currentNode == endNode)
+            //to avoid a length and smallest before setting check gscore of current, by the end
+            //the smallest will be the current
+            PathNode currentNode = openNodes[0];
+            for(int i = 0; i < openNodes.Count; i++)
+                if (openNodes[i].m_fCost < currentNode.m_fCost || (openNodes[i].m_fCost == currentNode.m_fCost && openNodes[i].m_hCost < currentNode.m_hCost))
+                    currentNode = openNodes[i];
+
+
+            openNodes.Remove(currentNode);
+            closedNodes.Add(currentNode);
+
+            if (currentNode.m_node == endNode)
             {
                 //We found the end node pathfinding is done
+                List<Vector3> path = new List<Vector3>();
+                //add the current/end node so that its just the parents being added in the loop
+                path.Add(currentNode.m_node.m_position);
+                while (currentNode.m_parent != null)
+                {
+                    path.Add(new Vector3(
+                        currentNode.m_parent.m_node.m_position.x,
+                        currentNode.m_parent.m_node.m_position.y,
+                        currentNode.m_parent.m_node.m_position.z));
+                    currentNode = currentNode.m_parent;
+                }
+                return path;
             }
 
-            //foreach connection to the current node
-            //  if the connection is not travelable or it is in closed
-            //    skip to the next connection
+            foreach(Node connection in currentNode.m_node.m_connectedNodes)
+            {
+                if (connection == null)
+                    continue;
+                //we need a travelable setting (nevermind this was a layermask for obsticles)
+
+                //this is a check to see if the node is in the closed or open list
+                bool isClosedNode = false;
+                foreach (PathNode closed in closedNodes)
+                {
+                    if (closed.m_node == connection)
+                    { 
+                        isClosedNode = true;
+                        break;
+                    } 
+                }
+                if (isClosedNode)
+                    continue;
+
+                bool isOpen = false;
+                foreach (PathNode open in openNodes)
+                {
+                    if (open.m_node == connection)
+                    {
+                        isOpen = true;
+                        break;
+                    }
+                }
+                if (isOpen)
+                    continue;
+
+                PathNode node = new PathNode(connection, currentNode);
+                node.m_gCost = Vector3.Distance(node.m_node.m_position, currentNode.m_node.m_position) + currentNode.m_gCost;
+
+                float distanceToConnection = currentNode.m_gCost + Vector3.Distance(currentNode.m_node.m_position, connection.m_position);
+                node.m_hCost = Vector3.Distance(node.m_node.m_position, endNode.m_position);
+                openNodes.Add(node);
+
+            }
+            //foreach connection to the current node DDDDD
+            //  if the connection is not travelable or it is in closed DDDD
+            //    skip to the next connection DDDDDDD
 
             // so from the looks of things each node carries the distance (fcost) and 
             // when it is checking connections it will also update to see if node to node is shorter than
@@ -79,14 +141,7 @@ public class AStar : MonoBehaviour
             //      add it to open
             
         }
+        //if it gets to this then we have failed
+        return null;
     }
 }
-
-/*
- * for the nodes we need
- * distance from start node
- * distance from end node
- * f cost = startdis + enddis
- * parent (pathfind)node
- * actual node itself
- */
---
+++ ComplexGameUnity/Assets/Scripts/Agent.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Agent : MonoBehaviour
+{
+    List<Vector3> path = new List<Vector3>();
+    private Vector3 start;
+    private Vector3 end;
+
+    public GameObject startObj = null;
+    public GameObject endObj = null;
+
+    LineRenderer line = null;
+    public Camera mainCam = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        line = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            if(Physics.Raycast(mainCam.ScreenPointToRay(Input.mousePosition), out hit, 9000.0f, ~0))
+            {
+                start = hit.point;
+               
+                var temp = AStar.FindClosestNode(hit.point);
+                startObj.transform.position = temp.m_position;
+            }  
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(mainCam.ScreenPointToRay(Input.mousePosition), out hit, 9000.0f, ~0))
+            {
+                end = hit.point;
+                
+                var temp = AStar.FindClosestNode(hit.point);
+                endObj.transform.position = temp.m_position;
+                
+            }
+        }
+        
+            if (start != null && end != null)
+            {
+                path = AStar.Pathfind(start, end);
+                line.positionCount = path.Count;
+                for(int i = 0; i < path.Count; i++)
+                {
+                    line.SetPosition(i, path[i]);
+                }
+            }
+        
+    }
+}
