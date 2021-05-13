c8c44d9df0249f89fe30a97fe9bd14a600f88b0d
Thomas Lamb <s200498@students.aie.edu.au>
2021-05-13 19:17:44 +1000

    Added agent spawner because of jesse complaining

---
+++ ComplexGameUnity/Assets/Scripts/AgentContainer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentContainer : MonoBehaviour
+{
+    public int amountToSpawn = 100;
+    public GameObject Agent;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            for (int i = 0; i < amountToSpawn; i++)
+                Instantiate(Agent, new Vector3(0, 0, 0), Quaternion.identity, transform);
+        }
+    }
+}
