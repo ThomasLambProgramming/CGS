a4cfae704d763ac02ea0ca2843ceeda8eef2f0d6
Thomas Lamb <s200498@students.aie.edu.au>
2021-06-01 21:46:23 +1000

    Made small test to try obstacle avoidance

---
+++ ComplexGameUnity/Assets/Scripts/Testing/Test.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    public Vector3 velocity;
+    private Vector3 startPosition;
+    private Quaternion startRotation;
+    private Rigidbody rb = null;
+    public Vector3 targetPosition;
+    public float maxMoveSpeed = 10f;
+    public float moveSpeed = 10f;
+    public float turnSpeed = 10f;
+    private bool avoiding = false;
+    void Start()
+    {
+        startPosition = transform.position;
+        startRotation = transform.rotation;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            var transform1 = transform;
+            transform1.position = startPosition;
+            transform1.rotation = startRotation;
+            rb.velocity = Vector3.zero;
+        }
+        
+        Vector3 direction = targetPosition - transform.position;
+        direction.Normalize();
+        direction *= moveSpeed;
+        
+        
+
+
+
+
+    }
+}
